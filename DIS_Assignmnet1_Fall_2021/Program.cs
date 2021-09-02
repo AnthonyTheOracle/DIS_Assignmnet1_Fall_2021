d080166e6941fe17586d7c950e420341bcf56ace
ekow <ekow@LAPTOP-CNSH7C6J>
2021-09-01 23:57:00 -0400

    Add project files.

---
+++ DIS_Assignmnet1_Fall_2021/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DIS_Assignmnet1_Fall_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question 1
+            Console.WriteLine("Q1 : Enter the string:");
+            string s = Console.ReadLine();
+            bool pos = HalvesAreAlike(s);
+            if (pos)
+            {
+                Console.WriteLine("Both Halfs of the string are alike");
+            }
+            else
+            {
+                Console.WriteLine("Both Halfs of the string are not alike");
+            }
+
+            Console.WriteLine();
+
+            //Question 2:
+            Console.WriteLine(" Q2 : Enter the string to check for pangram:");
+            string s1 = Console.ReadLine();
+            bool flag = CheckIfPangram(s1);
+            if (flag)
+            {
+                Console.WriteLine("Yes, the given string is a pangram");
+            }
+            else
+            {
+                Console.WriteLine("No, the given string is not a pangram");
+            }
+            Console.WriteLine();
+
+            //Question 3:
+            int[,] arr = new int[,] { { 1, 2, 3 }, { 3, 2, 1 } };
+            int Wealth = MaximumWealth(arr);
+            Console.WriteLine("Q3:");
+            Console.WriteLine("Richest person has a wealth of {0}", Wealth);
+
+            //Question 4:
+            string jewels = "aA";
+            string stones = "aAAbbbb";
+            Console.WriteLine("Q4:");
+            int num = NumJewelsInStones(jewels, stones);
+            Console.WriteLine("the number of stones you have that are also jewels are {0}:", num);
+
+            Console.WriteLine();
+
+            //Question 5:
+            Console.WriteLine("Q5:");
+            String word2 = "aiohn";
+            int[] indices = { 3, 1, 4, 2, 0 };
+            String rotated_string = RestoreString(word2, indices);
+            Console.WriteLine("The Final string after rotation is", rotated_string);
+
+
+            //Quesiton 6:
+            Console.WriteLine("Q6: Enter the sentence to convert:");
+            int[] nums = { 0, 1, 2, 3, 4 };
+            int[] index = { 0, 1, 2, 2, 1 };
+            int[] target = CreateTargetArray(nums, index);
+            Console.WriteLine("Target array  for the Given array's is:");
+            for (int i = 0; i < target.Length; i++)
+            {
+                Console.Write(target[i] + "\t");
+            }
+            Console.WriteLine();
+
+        }
+
+        //Q1
+        public static bool HalvesAreAlike(string s)
+        {
+            return true;
+        }
+
+        //Q2
+        public static bool CheckIfPangram(string sentence)
+        {
+            return true;
+        }
+
+        //Q3
+        public static int MaximumWealth(int[,] accounts)
+        {
+            return 0;
+        }
+
+        //Q4
+        public static int NumJewelsInStones(string jewels, string stones)
+        {
+            return 0;
+        }
+
+        //Q5
+        public static string RestoreString(string s, int[] indices)
+        {
+            return "";
+        }
+
+        //Q6
+        public static int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            return nums;
+        }
+
+    }
+}
+
